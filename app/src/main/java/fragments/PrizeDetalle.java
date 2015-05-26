c0e897546ed81739ac27ab85fe5f7dc2f1ea711b
Julian A Villaquiran <JULISCOUT00@HOTMAIL.COM>
2015-05-26 17:55:19 -0500

    Adding Funtionalities
    New Fragments and Activities added, still on prototype phase

--- app/src/main/java/fragments/PrizeDetalle.java
+++ app/src/main/java/fragments/PrizeDetalle.java
@@ -1,30 +1,25 @@
 package fragments;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 //import android.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import julianv.com.experience.PrizeDetail;
 import julianv.com.experience.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Premios.OnFragmentInteractionListener} interface
+ * {@link PrizeDetalle.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Premios#newInstance} factory method to
+ * Use the {@link PrizeDetalle#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Premios extends Fragment {
+public class PrizeDetalle extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -33,9 +28,6 @@ public class Premios extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private TextView t1;
-    private TextView t2;
-    private TextView t3;
 
     //private OnFragmentInteractionListener mListener;
 
@@ -45,11 +37,11 @@ public class Premios extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Premios.
+     * @return A new instance of fragment PrizeDetalle.
      */
     // TODO: Rename and change types and number of parameters
-    public static Premios newInstance() {
-        Premios fragment = new Premios();
+    public static PrizeDetalle newInstance() {
+        PrizeDetalle fragment = new PrizeDetalle();
         Bundle args = new Bundle();
         /*args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,7 +50,7 @@ public class Premios extends Fragment {
         return fragment;
     }
 
-    public Premios() {
+    public PrizeDetalle() {
         // Required empty public constructor
     }
 
@@ -69,50 +61,14 @@ public class Premios extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }*/
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View rootView = inflater.inflate(R.layout.fragment_premios, container, false);
-        t1 = (TextView) rootView.findViewById(R.id.prize1);
-        t2 = (TextView) rootView.findViewById(R.id.prize2);
-        t3 = (TextView) rootView.findViewById(R.id.prize3);
-
-        // create a listener
-        View.OnClickListener oclBtn = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //return new PrizeDetalle();
-                /*
-                // Create new fragment and transaction
-                Fragment newFragment = new PrizeDetalle();
-                // consider using Java coding conventions (upper first char class names!!!)
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
-                // Replace whatever is in the fragment_container view with this fragment,
-                // and add the transaction to the back stack
-                transaction.replace(R.id.pager, newFragment);
-                transaction.addToBackStack(null);
-
-                // Commit the transaction
-                transaction.commit();
-                */
-                startActivity(new Intent(getActivity(), PrizeDetail.class));
-
-            }
-        };
-
-        t1.setOnClickListener(oclBtn);
-        t2.setOnClickListener(oclBtn);
-        t3.setOnClickListener(oclBtn);
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_prize_detalle, container, false);
     }
-
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -135,7 +91,7 @@ public class Premios extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        //mListener = null;
+       // mListener = null;
     }
 
     /**
@@ -148,11 +104,11 @@ public class Premios extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    /*public interface OnFragmentInteractionListener {
+   /* public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
-    }*/
-
+    }
+    */
 
 
 }
